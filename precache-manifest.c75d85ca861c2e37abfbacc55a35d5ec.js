52b17a55e675ab9c40b739bf3e3a47d956eab261
leanhdw <41965441+leanhdw@users.noreply.github.com>
2020-05-11 12:03:13 -0400

    Add files via upload

---
+++ precache-manifest.c75d85ca861c2e37abfbacc55a35d5ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9af99dbf362ef8b49797",
+    "url": "/static/css/main.4c529bd7.chunk.css"
+  },
+  {
+    "revision": "9af99dbf362ef8b49797",
+    "url": "/static/js/main.aeaa4c02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6b4e819748445977a29",
+    "url": "/static/css/2.8ef2d315.chunk.css"
+  },
+  {
+    "revision": "f6b4e819748445977a29",
+    "url": "/static/js/2.daf7d3d5.chunk.js"
+  },
+  {
+    "revision": "1e2c16fec6f26a0c1dc88791e30ce84c",
+    "url": "/static/media/instruction2-image1.1e2c16fe.png"
+  },
+  {
+    "revision": "40f25c73ee1eea0e85e90032c5d264a5",
+    "url": "/static/media/instruction3-image1.40f25c73.png"
+  },
+  {
+    "revision": "84a1d5b1fa26051484cbedf9ace9141b",
+    "url": "/static/media/ajax-loader.84a1d5b1.gif"
+  },
+  {
+    "revision": "3f5d2a79fb153cbf7d7db45e72cca0cb",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.d78b28982b205f491619094f4c0820e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f0f40ce109480c1d64a",
+    "url": "/static/css/main.4c529bd7.chunk.css"
+  },
+  {
+    "revision": "9f0f40ce109480c1d64a",
+    "url": "/static/js/main.79b18a9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6b4e819748445977a29",
+    "url": "/static/css/2.8ef2d315.chunk.css"
+  },
+  {
+    "revision": "f6b4e819748445977a29",
+    "url": "/static/js/2.daf7d3d5.chunk.js"
+  },
+  {
+    "revision": "1e2c16fec6f26a0c1dc88791e30ce84c",
+    "url": "/static/media/instruction2-image1.1e2c16fe.png"
+  },
+  {
+    "revision": "40f25c73ee1eea0e85e90032c5d264a5",
+    "url": "/static/media/instruction3-image1.40f25c73.png"
+  },
+  {
+    "revision": "84a1d5b1fa26051484cbedf9ace9141b",
+    "url": "/static/media/ajax-loader.84a1d5b1.gif"
+  },
+  {
+    "revision": "bbc1c90a721a9c48941ca5a1915e204d",
+    "url": "/index.html"
+  }
+];
